f4c8a480fc87631b1562e961abddbdcfd82ebd8b
MatanMarko <120421283+MatanMarko@users.noreply.github.com>
2025-01-01 10:51:34 +0200

    fixed charactermovement

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     public float moveSpeed = 5f; // Speed of the character
-    [SerializeField] Animator animator; // Animator for handling animations
+
+    [SerializeField]
+    private Animator animator; // Animator for handling animations
 
     private Vector2 movement; // Stores the player's input
 
@@ -14,15 +16,16 @@ public class CharacterMovement : MonoBehaviour
         movement.y = Input.GetAxisRaw("Vertical");   // W/S or Up/Down
 
         // Tell the Animator about the movement
-        try{
+        try
+        {
             animator.SetFloat("MoveX", movement.x);
             animator.SetFloat("MoveY", movement.y);
             animator.SetBool("IsMoving", movement != Vector2.zero);
         }
-        catch{
+        catch
+        {
             Debug.Log("Animator not found");
         }
-        
 
         // Move the character
         transform.Translate(movement * moveSpeed * Time.deltaTime);
