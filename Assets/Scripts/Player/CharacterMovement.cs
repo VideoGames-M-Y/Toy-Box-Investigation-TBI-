6ee7aca26f9144433b7dcb0cd6de4606ad67f94f
MatanMarko <120421283+MatanMarko@users.noreply.github.com>
2025-01-08 00:11:51 +0200

    updatedForLinter

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -7,25 +7,29 @@ public class CharacterMovement : MonoBehaviour
 
     private Vector2 movement; // Stores the player's input
 
-    // void Update()
-    // {
-    //     // Get player input
-    //     movement.x = Input.GetAxisRaw("Horizontal"); // A/D or Left/Right
-    //     movement.y = Input.GetAxisRaw("Vertical");   // W/S or Up/Down
-
-    //     // Tell the Animator about the movement
-    //     try{
-    //         animator.SetFloat("MoveX", movement.x);
-    //         animator.SetFloat("MoveY", movement.y);
-    //         animator.SetBool("IsMoving", movement != Vector2.zero);
-    //     }
-    //     catch{
-    //         Debug.Log("Animator not found");
-    //     }
-        
-    //     // Move the character
-    //     transform.Translate(movement * moveSpeed * Time.deltaTime);
-    // }
+    /*
+     * // void Update()
+     * // {
+     * //     // Get player input
+     * //     movement.x = Input.GetAxisRaw("Horizontal"); // A/D or Left/Right
+     * //     movement.y = Input.GetAxisRaw("Vertical");   // W/S or Up/Down
+     * //
+     * //     // Tell the Animator about the movement
+     * //     try
+     * //     {
+     * //         animator.SetFloat("MoveX", movement.x);
+     * //         animator.SetFloat("MoveY", movement.y);
+     * //         animator.SetBool("IsMoving", movement != Vector2.zero);
+     * //     }
+     * //     catch
+     * //     {
+     * //         Debug.Log("Animator not found");
+     * //     }
+     * //
+     * //     // Move the character
+     * //     transform.Translate(movement * moveSpeed * Time.deltaTime);
+     * // }
+     */
 
     void Update()
     {
